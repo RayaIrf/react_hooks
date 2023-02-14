bb13122deff36181432eb91fb59f507522998174
Raya Irfansyah <xrayirf@gmail.com>
2023-02-14 13:30:46 +0700

    useState oject

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HookCounterTwo />
+      <HookCounterThree />
     </div>
   );
 }
