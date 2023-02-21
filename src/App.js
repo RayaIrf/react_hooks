283292979a8c68e6e6dc39b2de3cf231b4c3ff57
Raya Irfansyah <xrayirf@gmail.com>
2023-02-21 13:27:47 +0700

    Run effects only once

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ import HookCounterFour from './components/HookCounterFour';
 import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne';
 import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <ClassMouse />
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
--- src/components/HookMouse.js
+++ src/components/HookMouse.js
@@ -5,10 +5,16 @@ function HookMouse() {
     const [x, setX] = useState(0)
     const [y, setY] = useState(0)
 
+    const logMousePosition = e => {
+        console.log('Mouse event');
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+
     useEffect(() => {
         console.log('useEffect called');
-        window.addEventListener('mousemove', log)
-    })
+        window.addEventListener('mousemove', logMousePosition)
+    }, [])
 
   return (
     <div>
